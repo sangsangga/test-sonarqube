34ffa01f0d3ec6c64e7aee32fbb1483f32d329f4
sangsangga <wandaputrasangga@gmail.com>
2023-07-11 15:25:13 +0700

    copy paste

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -78,5 +78,18 @@ public class DemoApplication {
 		return "SMA";
 	}
 
+	public String myAge () {
+		int age = 18;
+
+		if (age < 5 ) {
+			return "Balita";
+		} else if (age > 6 && age <= 12) {
+			return "SD";
+		} else if (age > 12 && age < 15) {
+			return "SMP";
+		}
+		return "SMA";
+	}
+
 
 }
